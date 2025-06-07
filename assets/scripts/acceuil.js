2c7e55e4dcdcbb556e72f5fcc6b7822cf23a4701
ATechproC <choraichianass@gmail.com>
2025-06-07 19:32:13 +0100

    Update acceuil.js

--- assets/scripts/acceuil.js
+++ assets/scripts/acceuil.js
@@ -1,17 +1,31 @@
-let containerSwitch = document.getElementById("containerSwitch");
-let btnSwitch = document.getElementById("btnSwitch");
-let message = document.getElementById("message");
-let marquee = document.getElementById("marquee");
-let text= document.getElementById("text");
+let darkmode = localStorage.getItem("darkmode");
+let themeSwitch = document.getElementById("btnSwitch");
 
+const enableDarkmode = () => {
+    btnSwitch.classList.add("left");
+    btnSwitch.classList.add("white");
+    containerSwitch.classList.add("black");
+    document.body.classList.add("white");
+    message.classList.add("color");
+    text.classList.add("textColor");
+    localStorage.setItem("darkmode", "active");
+}
 
-btnSwitch.addEventListener("click", function () {
-    this.classList.toggle("left");
-    this.classList.toggle("white");
-    containerSwitch.classList.toggle("black");
-    document.body.classList.toggle("white");
-    message.classList.toggle("color");
-    text.classList.toggle("textColor");
+const disableDarkmode = () => {
+    btnSwitch.classList.remove("left");
+    btnSwitch.classList.remove("white");
+    containerSwitch.classList.remove("black");
+    document.body.classList.remove("white");
+    message.classList.remove("color");
+    text.classList.remove("textColor");
+    localStorage.setItem("darkmode", "null");
+}
+
+if(darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 });
 
 
